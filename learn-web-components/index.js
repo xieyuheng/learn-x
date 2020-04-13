47d405eb454551b005262e617f1d06870e1083b8
Xie Yuheng <xyheme@gmail.com>
2020-04-13 10:46:40 +0800

    up

--- learn-web-components/index.js
+++ learn-web-components/index.js
@@ -19,18 +19,65 @@ function elm_strict(tag, attr, children) {
 function elm(...args) {
   if (args.length === 0)
     throw new Error("args.length === 0\n")
-  else if (args.length === 1)
-    return elm_strict(args[0], {}, [])
-  else if (args.length === 2)
-    return (args[1] instanceof Array)
-    ? elm_strict(args[0], {}, args[1])
-    : elm_strict(args[0], args[1], [])
-  else if (args.length === 3)
-    return elm_strict(args[0], args[1], args[2])
-  else
-    throw new Error("args.length > 3\n")
+  else {
+    const tag = tag_class_p(args[0]) ? tag_gen(args[0]) : args[0]
+    if (args.length === 1)
+      return elm_strict(tag, {}, [])
+    else if (args.length === 2)
+      return (args[1] instanceof Array)
+      ? elm_strict(tag, {}, args[1])
+      : elm_strict(tag, args[1], [])
+    else if (args.length === 3)
+      return elm_strict(tag, args[1], args[2])
+    else
+      throw new Error("args.length > 3\n")
+  }
 }
 
+/**
+ * @arg: {class} tag_class
+ */
+function tag_register(tag_class) {
+  const tag = tag_name_gen(tag_class)
+  const found = window.customElements.get(tag)
+  if (found === undefined)
+    window.customElements.define(tag, tag_class)
+  else {
+    if (found !== tag_class)
+      throw new Error(
+        "re-register tag to different class\n" +
+          `tag: ${tag}\n` +
+          `class: ${tag_class}\n`)
+  }
+}
+
+/**
+ * @arg: {any} x
+ * @return: {boolean}
+ */
+function tag_class_p(x) {
+  return typeof x === "function"
+}
+
+/**
+ * @arg: {class} tag_class
+ * @return: {string}
+ */
+function tag_name_gen(tag_class) {
+  const tag_name = tag_class.name.split("_").join("-")
+  return tag_name
+}
+
+/**
+ * @arg: {class} tag_class
+ * @return: {string}
+ */
+function tag_gen(tag_class) {
+  tag_register(tag_class)
+  return tag_name_gen(tag_class)
+}
+
+// examples
 
 class roadmap_usage_t extends HTMLElement {
   constructor() {
@@ -45,13 +92,15 @@ class roadmap_usage_t extends HTMLElement {
   }
 }
 
-customElements.define("roadmap-usage-t", roadmap_usage_t)
+tag_register(roadmap_usage_t)
+tag_register(roadmap_usage_t)
+tag_register(roadmap_usage_t)
 
 class roadmap_main_t extends HTMLElement {
   constructor() {
     super()
     const shadow = this.attachShadow({ mode: "open" })
-    shadow.append(elm("roadmap-usage-t", [
+    shadow.append(elm(roadmap_usage_t, [
       elm("div", { "slot": "url" }, [ "http://example.com" ]),
       elm("div", { "slot": "x" }, [ "xxx" ]),
       elm("div", { "slot": "y" }, [ "yyy" ]),
@@ -59,4 +108,4 @@ class roadmap_main_t extends HTMLElement {
   }
 }
 
-customElements.define("roadmap-main-t", roadmap_main_t)
+tag_register(roadmap_main_t)
