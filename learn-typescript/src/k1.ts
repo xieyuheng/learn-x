ae3111ccf2555d0fe6f460231feb9d264011a637
Xie Yuheng <xyheme@gmail.com>
2020-11-26 22:16:09 +0800

    up

---
+++ learn-typescript/src/k1.ts
@@ -0,0 +1,23 @@
+// interface A {
+//   x: string
+//   y: string
+// }
+
+// interface B extends A {
+//   z: string
+// }
+
+type A = {
+  x: string
+  y: string
+}
+
+type B = A & {
+  z: string
+}
+
+const b: B = {
+  x: "",
+  y: "",
+  z: "",
+}
