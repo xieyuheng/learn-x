0a2019f06504f7b5bdab13860a6210ad9773b422
Xie Yuheng <xyheme@gmail.com>
2020-11-28 17:18:53 +0800

    up

---
+++ learn-object-oriented-design/typescript/src/gear.ts
@@ -0,0 +1,15 @@
+export class Gear {
+  constructor(
+    public chainring: number,
+    public cog: number,
+    public rim: number,
+    public tire: number
+  ) {}
+
+  get ratio(): number {
+    return this.chainring / this.cog
+  }
+}
+
+console.log(new Gear(52, 11).ratio)
+console.log(new Gear(30, 27).ratio)
