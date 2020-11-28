55001698700dda744d0675338ee447a130a47a53
Xie Yuheng <xyheme@gmail.com>
2020-11-28 17:43:26 +0800

    up

--- learn-object-oriented-design/typescript/src/gear.ts
+++ learn-object-oriented-design/typescript/src/gear.ts
@@ -9,7 +9,13 @@ export class Gear {
   get ratio(): number {
     return this.chainring / this.cog
   }
+
+  get gear_inches(): number {
+    return this.ratio * (this.rim + this.tire * 2)
+  }
 }
 
-console.log(new Gear(52, 11, 26, 1.5).ratio)
-console.log(new Gear(52, 11, 24, 1.25).ratio)
+// console.log(new Gear(52, 11).ratio)
+// console.log(new Gear(30, 27).ratio)
+console.log(new Gear(52, 11, 26, 1.5).gear_inches)
+console.log(new Gear(52, 11, 24, 1.25).gear_inches)
