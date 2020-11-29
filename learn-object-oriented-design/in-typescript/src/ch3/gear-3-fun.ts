f810856a110b440feaf8f6ee959b212f76259d4e
Xie Yuheng <xyheme@gmail.com>
2020-11-29 17:46:56 +0800

    up

---
+++ learn-object-oriented-design/in-typescript/src/ch3/gear-3-fun.ts
@@ -0,0 +1,49 @@
+type Gear = {
+  chainring: number
+  cog: number
+  wheel: { diameter: number }
+  ratio: number
+  gear_inches: number
+}
+
+function Gear(
+  chainring: number,
+  cog: number,
+  wheel: { diameter: number }
+): Gear {
+  const ratio = chainring / cog
+  const gear_inches = ratio * wheel.diameter
+
+  return {
+    chainring,
+    cog,
+    wheel,
+    ratio,
+    gear_inches,
+  }
+}
+
+type Wheel = {
+  rim: number
+  tire: number
+  diameter: number
+  circumference: number
+}
+
+function Wheel(rim: number, tire: number): Wheel {
+  const diameter = rim + tire * 2
+  const circumference = diameter * Math.PI
+
+  return {
+    rim,
+    tire,
+    diameter,
+    circumference,
+  }
+}
+
+console.log(Gear(52, 11, Wheel(26, 1.5)).gear_inches)
+console.log(Gear(52, 11, Wheel(24, 1.25)).gear_inches)
+
+console.log(Wheel(26, 1.5).circumference)
+console.log(Wheel(24, 1.25).circumference)
