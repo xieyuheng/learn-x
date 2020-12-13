9bca4e64aa87e50f15f4025989841fb4cc41fa02
Xie Yuheng <xyheme@gmail.com>
2020-12-13 17:35:28 +0800

    up

--- learn-rails/blog/config/routes.rb
+++ learn-rails/blog/config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  resources :articles
 end
