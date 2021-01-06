ffa5c4a96bf77291c16232530106ddd660911dc4
Xie Yuheng <xyheme@gmail.com>
2021-01-06 15:27:00 +0800

    up

--- learn-tailwindcss/tailwind-vue-app/src/main.ts
+++ learn-tailwindcss/tailwind-vue-app/src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
-import "./assets/styles/index.css"
+import "./assets/styles/tailwind.css"
 
 Vue.config.productionTip = false
 
