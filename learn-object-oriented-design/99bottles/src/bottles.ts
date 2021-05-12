985bb78664e8d1a3d405c617bb83d90e876e1ffb
Xie Yuheng <xyheme@gmail.com>
2021-05-12 19:39:37 +0800

    up

--- learn-object-oriented-design/99bottles/src/bottles.ts
+++ learn-object-oriented-design/99bottles/src/bottles.ts
@@ -13,33 +13,28 @@ export class Bottles {
   }
 
   verse(n: number): string {
-    if (n === 0) {
-      return (
-        `No more bottles of milk on the wall, ` +
-        `no more bottles of milk.\n` +
-        `Go to the store and buy some more, ` +
-        `99 bottles of milk on the wall.\n`
-      )
-    } else {
-      return (
-        `${n} ${this.container(n)} of milk on the wall, ` +
-        `${n} ${this.container(n)} of milk.\n` +
-        this.ending(n - 1)
-      )
-    }
-  }
-
-  ending(n: number): string {
-    if (n === 0) {
-      return (
-        `Take it down and pass it around, ` +
-        `no more bottles of milk on the wall.\n`
-      )
-    } else {
-      return (
-        `Take one down and pass it around, ` +
-        `${n} ${this.container(n)} of milk on the wall.\n`
-      )
+    switch (n) {
+      case 0:
+        return (
+          `No more bottles of milk on the wall, ` +
+          `no more bottles of milk.\n` +
+          `Go to the store and buy some more, ` +
+          `99 bottles of milk on the wall.\n`
+        )
+      case 1:
+        return (
+          `${n} ${this.container(n)} of milk on the wall, ` +
+          `${n} ${this.container(n)} of milk.\n` +
+          `Take it down and pass it around, ` +
+          `no more bottles of milk on the wall.\n`
+        )
+      default:
+        return (
+          `${n} ${this.container(n)} of milk on the wall, ` +
+          `${n} ${this.container(n)} of milk.\n` +
+          `Take one down and pass it around, ` +
+          `${n - 1} ${this.container(n - 1)} of milk on the wall.\n`
+        )
     }
   }
 
