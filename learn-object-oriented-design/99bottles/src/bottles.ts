5f7d49357730badbf48d9d413d63399036437431
Xie Yuheng <xyheme@gmail.com>
2021-05-13 19:45:32 +0800

    remove case

--- learn-object-oriented-design/99bottles/src/bottles.ts
+++ learn-object-oriented-design/99bottles/src/bottles.ts
@@ -21,15 +21,6 @@ export class Bottles {
           `Go to the store and buy some more, ` +
           `99 bottles of milk on the wall.\n`
         )
-      case 1:
-        return (
-          `${n} ${this.container(n)} of milk on the wall, ` +
-          `${n} ${this.container(n)} of milk.\n` +
-          `Take ${this.pronoun(n)} down and pass it around, ` +
-          `${this.quantity(n - 1)} ${this.container(
-            n - 1
-          )} of milk on the wall.\n`
-        )
       default:
         return (
           `${n} ${this.container(n)} of milk on the wall, ` +
