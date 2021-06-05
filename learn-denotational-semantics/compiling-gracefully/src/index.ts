51877492eac50becdb4e67d80722eb7f923638d4
Xie Yuheng <xyheme@gmail.com>
2021-06-05 17:09:40 +0800

    up

--- learn-denotational-semantics/compiling-gracefully/src/index.ts
+++ learn-denotational-semantics/compiling-gracefully/src/index.ts
@@ -1,3 +1,5 @@
+export type Fn<A, B> = (a: A) => B
+
 export class StackFn<A, B, Z> {
   stack_fn: ([a, z]: [A, Z]) => [B, Z]
 
@@ -5,7 +7,7 @@ export class StackFn<A, B, Z> {
     this.stack_fn = stack_fn
   }
 
-  static from_fn<A, B, Z>(fn: (a: A) => B): StackFn<A, B, Z> {
+  static from_fn<A, B, Z>(fn: Fn<A, B>): StackFn<A, B, Z> {
     return new StackFn(([a, z]) => [fn(a), z])
   }
 }
