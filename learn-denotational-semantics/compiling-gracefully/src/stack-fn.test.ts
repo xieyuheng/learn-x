5286cdafee2acdce58ab1701dbf619833daf8be1
Xie Yuheng <xyheme@gmail.com>
2021-06-05 17:25:50 +0800

    up

--- learn-denotational-semantics/compiling-gracefully/src/stack-fn.test.ts
+++ learn-denotational-semantics/compiling-gracefully/src/stack-fn.test.ts
@@ -1,8 +1,1 @@
 import { StackFn, Fn } from "./stack-fn"
-
-function id_fn<A>(): Fn<A, A> {
-  return (a) => a
-}
-
-console.log(StackFn.from_fn(id_fn))
-console.log(StackFn.from_fn((x: number) => x + x))
--- learn-denotational-semantics/compiling-gracefully/src/stack-fn.ts
+++ learn-denotational-semantics/compiling-gracefully/src/stack-fn.ts
@@ -1,3 +1,5 @@
+import { Category } from "./category"
+
 export type Fn<A, B> = (a: A) => B
 
 export class StackFn<A, B, Z> {
@@ -10,4 +12,19 @@ export class StackFn<A, B, Z> {
   static from_fn<A, B, Z>(fn: Fn<A, B>): StackFn<A, B, Z> {
     return new StackFn(([a, z]) => [fn(a), z])
   }
+
+  static id<A, Z>(): StackFn<A, A, Z> {
+    return StackFn.from_fn(id_fn())
+  }
 }
+
+const stack_fn_category: Category<> = new Category({
+  
+})
+  
+function id_fn<A>(): Fn<A, A> {
+  return (a) => a
+}
+
+console.log(StackFn.from_fn(id_fn))
+console.log(StackFn.from_fn((x: number) => x + x))
