9372d6092999a003893716a03d1b4c99b6b76fd9
Xie Yuheng <xyheme@gmail.com>
2021-08-03 21:22:53 +0800

    up

---
+++ learn-zmq/play-with-zmq/src/pull.play.ts
@@ -0,0 +1,14 @@
+import * as Zmq from "zeromq"
+
+async function run(): Promise<void> {
+  const sock = new Zmq.Pull()
+
+  sock.connect("tcp://127.0.0.1:3000")
+  console.log("Worker connected to port 3000")
+
+  for await (const [msg] of sock) {
+    console.log("work: %s", msg.toString())
+  }
+}
+
+run()
