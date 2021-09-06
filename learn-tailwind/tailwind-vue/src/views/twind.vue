bed075e72a2463be221e5de2673c02a40ed3770d
Xie Yuheng <xyheme@gmail.com>
2021-09-06 16:59:06 +0800

    up

--- learn-tailwind/tailwind-vue/src/views/twind.vue
+++ learn-tailwind/tailwind-vue/src/views/twind.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-    <hi-twind />
-    <hiya-twind />
+    <div>
+      <h1>Normal import:</h1>
+      <hi-twind />
+      <hiya-twind />
+    </div>
+
+    <div>
+      <h1>Web Component:</h1>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator"
 import twindComponents from "@xieyuheng/twind-component-example"
+import twc from "@xieyuheng/twind-component-example/wc/twc"
 
 @Component({
   components: {
     ...twindComponents,
   },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  mounted(): void {
+    console.log(twc)
+  }
+}
 </script>
