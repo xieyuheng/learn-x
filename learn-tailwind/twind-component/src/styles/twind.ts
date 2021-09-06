fa9463f00dff9f6ed601d0dcca2e3841d104feac
Xie Yuheng <xyheme@gmail.com>
2021-09-06 17:34:55 +0800

    up

--- learn-tailwind/twind-component/src/styles/twind.ts
+++ learn-tailwind/twind-component/src/styles/twind.ts
@@ -1,10 +1,7 @@
 import { create, cssomSheet } from "twind"
 import * as colors from "twind/colors"
 
-const sheet = cssomSheet({ target: new CSSStyleSheet() })
-
 export const { tw } = create({
-  sheet,
   theme: {
     extend: {
       colors,
