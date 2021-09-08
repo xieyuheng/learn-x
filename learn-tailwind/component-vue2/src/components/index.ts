51279c1202dfa18c723bd60c576b935130fd1e11
Xie Yuheng <xyheme@gmail.com>
2021-09-08 19:47:21 +0800

    up

--- learn-tailwind/component-vue2/src/components/index.ts
+++ learn-tailwind/component-vue2/src/components/index.ts
@@ -1,4 +1,6 @@
 export default {
-  "hi-twind": require("./hi-twind.vue").default,
-  "hiya-twind": require("./hiya-twind.vue").default,
+  // "hi-twind": require("./hi-twind.vue").default,
+  // "hiya-twind": require("./hiya-twind.vue").default,
+  "hi-windi": require("./hi-windi.vue").default,
+  "hiya-windi": require("./hiya-windi.vue").default,
 }
--- learn-tailwind/component-vue2/src/main.ts
+++ learn-tailwind/component-vue2/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue"
 import App from "./app.vue"
+import "windi.css"
 
 Vue.config.productionTip = false
 
