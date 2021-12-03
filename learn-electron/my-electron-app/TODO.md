1bd6c323c108273c868ce5cb844d513e2b0e38e9
Xie Yuheng <xyheme@gmail.com>
2021-12-03 16:21:30 +0800

    - try uikit

--- learn-electron/my-electron-app/TODO.md
+++ learn-electron/my-electron-app/TODO.md
@@ -1,10 +1,3 @@
-- use vue in electron
-
-- use tailwind in electron
-
-- use uikit in electron
-  - https://getuikit.com/docs
-
 - use `electron-builder`
 
   - [maybe] remove `electron-forge`
--- learn-electron/my-electron-app/src/main.ts
+++ learn-electron/my-electron-app/src/main.ts
@@ -30,3 +30,10 @@ async function start(): Promise<void> {
 }
 
 start()
+
+if (env === "development") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+    hardResetMethod: "exit",
+  })
+}
