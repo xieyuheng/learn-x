d1894656b327af68753da5ecc832322aac108693
Xie Yuheng <xyheme@gmail.com>
2022-03-12 14:52:19 +0800

    up

--- learn-scheme/hello-world.scm
+++ learn-scheme/hello-world.scm
@@ -1,10 +1,2 @@
-(import (scheme base)
-        (scheme write))
-
-(define-syntax let1
-  (syntax-rules ()
-    ((let ((var expr) ...) body ...)
-     ((lambda (var ...) body ...) expr ...))))
-
-(write (let1 ((x "hello world"))
-           x))
+(write
+ (let ([x '(hi)]) x))
