7c321cb6d0593be3c822a354fb7d8633cee4db5e
Xie Yuheng <xyheme@gmail.com>
2022-04-05 03:01:32 +0800

    up

--- learn-coq/cpdt/universes.v
+++ learn-coq/cpdt/universes.v
@@ -10,3 +10,22 @@ Set Printing Universes.
 Check Set.
 Check Prop.
 Check Type.
+
+Check forall T: Set, T.
+Check forall T: Type, T.
+
+Definition id (T: Set) (x: T): T := x.
+
+Check id nat 0.
+(* Check id Set nat. *)
+
+Reset id.
+Definition id (T: Type) (x: T): T := x.
+Check id nat 0.
+Check id Set nat.
+Check id Type Type.
+
+Check forall T: Type, T -> T.
+Check id (forall T: Type, T -> T) id.
+
+(* TODO *)
