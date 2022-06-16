f834ddd97ad99504cc8232eb152c138cbec89262
Xie Yuheng <xyheme@gmail.com>
2022-06-16 14:17:52 +0800

    up

--- learn-react/beginning-react/src/hooks/useLocalStorage.js
+++ learn-react/beginning-react/src/hooks/useLocalStorage.js
@@ -10,5 +10,5 @@ export default function useLocalStorage(key, initValue) {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
-  return [value, setValue]
+  return [value, setValue];
 }
