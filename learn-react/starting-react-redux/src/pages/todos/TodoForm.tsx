1cc51e6165db1660351278b8a9888aca41c76367
Xie Yuheng <xyheme@gmail.com>
2022-06-24 17:58:15 +0800

    - `TodoForm` -- extract `addTodo`

--- learn-react/starting-react-redux/src/pages/todos/TodoForm.tsx
+++ learn-react/starting-react-redux/src/pages/todos/TodoForm.tsx
@@ -5,34 +5,25 @@ import * as state from './todoSlice';
 export default function TodoForm() {
   const dispatch = useDispatch();
   const todos = useSelector(state.selectors.todos);
-  const [todoInput, setTodoInput] = useState('');
-
-  function freshId() {
-    const ids = todos.map((todo) => todo.id);
-    if (ids.length === 0) return 0;
-    return Math.max(...ids) + 1;
-  }
-
-  function addTodo(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
-    if (!todoInput.trim()) return;
-    const newTodo = { id: freshId(), title: todoInput, isComplete: false };
-    dispatch(state.actions.setTodos([newTodo, ...todos]));
-    setTodoInput('');
-  }
-
-  function handleInput(event: React.ChangeEvent<HTMLInputElement>): void {
-    setTodoInput(event.target.value);
-  }
+  const todoInput = useSelector(state.selectors.todoInput);
 
   return (
-    <form className="border-2" action="#" onSubmit={(event) => addTodo(event)}>
+    <form
+      className="border-2"
+      action="#"
+      onSubmit={(event) => {
+        event.preventDefault();
+        dispatch(state.actions.addTodo());
+      }}
+    >
       <input
         className="p-2"
         type="text"
         placeholder="Add todo"
         value={todoInput}
-        onChange={(event) => handleInput(event)}
+        onChange={(event) =>
+          dispatch(state.actions.setTodoInput(event.target.value))
+        }
       />
     </form>
   );
--- learn-react/starting-react-redux/src/pages/todos/todoSlice.ts
+++ learn-react/starting-react-redux/src/pages/todos/todoSlice.ts
@@ -10,11 +10,13 @@ export interface Todo {
 export interface TodoState {
   todos: Array<Todo>;
   filterName: string;
+  todoInput: string;
 }
 
 const initialState: TodoState = {
   todos: [],
   filterName: 'all',
+  todoInput: '',
 };
 
 function setTodos(state: TodoState, todos: Array<Todo>) {
@@ -23,6 +25,10 @@ function setTodos(state: TodoState, todos: Array<Todo>) {
 
 function setFilterName(state: TodoState, filterName: string) {
   state.filterName = filterName;
+}
+
+function setTodoInput(state: TodoState, todoInput: string) {
+  state.todoInput = todoInput;
 }
 
 function clearCompleted(state: TodoState) {
@@ -64,18 +70,38 @@ function updateTodo(state: TodoState, id: number, title: string) {
   });
 }
 
+function freshId(todos: Array<Todo>): number {
+  const ids = todos.map((todo) => todo.id);
+  if (ids.length === 0) return 0;
+  return Math.max(...ids) + 1;
+}
+
+function addTodo(state: TodoState): void {
+  if (!state.todoInput.trim()) return;
+  const newTodo = {
+    id: freshId(state.todos),
+    title: state.todoInput,
+    isComplete: false,
+  };
+
+  setTodos(state, [newTodo, ...state.todos]);
+  setTodoInput(state, '');
+}
+
 export const slice = createSlice({
   name: 'todos',
   initialState,
   reducers: prepareReducers({
     setTodos,
     setFilterName,
+    setTodoInput,
     clearCompleted,
     checkAll,
     deleteTodo,
     completeTodo,
     editingTodo,
     updateTodo,
+    addTodo,
   }),
 });
 
@@ -86,6 +112,7 @@ export const actions: any = slice.actions;
 export const selectors = {
   todos: ({ todos }: { todos: TodoState }) => todos.todos,
   filterName: ({ todos }: { todos: TodoState }) => todos.filterName,
+  todoInput: ({ todos }: { todos: TodoState }) => todos.todoInput,
   remaining: ({ todos }: { todos: TodoState }) =>
     todos.todos.filter((todo: Todo) => !todo.isComplete).length,
   todosFiltered: ({ todos }: { todos: TodoState }) => {
