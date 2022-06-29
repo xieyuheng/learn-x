abef25fa555373102f73c638dfea1597a6fe8edc
Xie Yuheng <xyheme@gmail.com>
2022-06-29 16:56:40 +0800

    up

--- learn-nextjs/nextjs-blog/pages/index.js
+++ learn-nextjs/nextjs-blog/pages/index.js
@@ -1,12 +1,27 @@
 import Link from "next/link"
+import Head from "next/head"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <h1 className="title">
-      Read{" "}
-      <Link href="/posts/first-post">
-        <a className="underline">this page!</a>
-      </Link>
-    </h1>
+    <>
+      <Head>
+        <title>NextJs Blog</title>
+      </Head>
+
+      <h1 className="title">
+        Read{" "}
+        <Link href="/posts/first-post">
+          <a className="underline">this page!</a>
+        </Link>
+        <img className="h-20" src="/images/profile.jpg" alt="Your Name" />
+        <Image
+          src="/images/profile.jpg"
+          height={256}
+          width={256}
+          alt="Your Name"
+        />
+      </h1>
+    </>
   )
 }
