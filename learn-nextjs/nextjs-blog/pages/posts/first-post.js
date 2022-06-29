54893db8ad8480d695dc1e462bbb0b8cff6f87f1
Xie Yuheng <xyheme@gmail.com>
2022-06-29 17:11:39 +0800

    up

--- learn-nextjs/nextjs-blog/pages/posts/first-post.js
+++ learn-nextjs/nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,16 @@
 import Link from "next/link"
+import Head from "next/head"
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
-          <a className="underline">Back to home</a>
+          <a>Back to home</a>
         </Link>
       </h2>
     </>
