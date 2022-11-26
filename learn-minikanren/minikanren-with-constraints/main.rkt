bfae3f90f711663056c659c19a54c86d77c2b32d
Xie Yuheng <xyheme@gmail.com>
2022-11-27 06:10:56 +0800

    up

--- learn-minikanren/minikanren-with-constraints/main.rkt
+++ learn-minikanren/minikanren-with-constraints/main.rkt
@@ -1,5 +1,3 @@
 #lang racket
 
 (require (file "minikanren.rkt"))
-
-(display make-st)
--- learn-minikanren/minikanren-with-constraints/minikanren.rkt
+++ learn-minikanren/minikanren-with-constraints/minikanren.rkt
@@ -22,3 +22,134 @@
 (define mzero '())
 
 (define (unit st) (cons st mzero))
+
+(define (ext-S u v S) '((,u . ,v) . ,S))
+
+(define (walk u S)
+  (let ([pr (and (var? u) (assoc u S var=?))])
+    (if pr
+      (walk (cdr pr) S)
+      u)))
+
+(define (unify u v S)
+  (let ([u (walk u S)]
+        [v (walk v S)])
+    (cond
+      [(and (var? u) (var? v) (var=? u v)) S]
+      [(var? u) (ext-S u v S)]
+      [(var? v) (ext-S v u S)]
+      [(and (pair? u) (pair? v))
+       (let ([S (unify (car u) (car v) S)])
+         (and S (unify (cdr u) (cdr v) S)))]
+      [else (and (eqv? u v) S)])))
+
+(define (== u v)
+  (lambda (st)
+    (let ([S (unify u v (S-of st))])
+      (if S (cons (make-st S (C-of st)) mzero) mzero))))
+
+(define-syntax fresh
+  (syntax-rules ()
+    [(_ () g0 g ...) (conj+ g0 g ...)]
+    [(_ (x0 x ...) g0 g ...)
+     (call/fresh (lambda (x0) (fresh (x ...) g0 g ...)))]))
+
+(define (call/fresh f)
+  (lambda (st)
+    (let ([C (C-of st)])
+      ((f (var C)) (make-st (S-of st) (+ C 1))))))
+
+(define (disj g1 g2)
+  (lambda (st)
+    (mplus (g1 st) (g2 st))))
+
+(define (conj g1 g2)
+  (lambda (st)
+    (bind (g1 st) g2)))
+
+(define (mplus $1 $2)
+  (cond
+    [(null? $1) $2]
+    [(procedure? $1) (lambda () (mplus $2 ($1)))]
+    [(pair? $1) (cons (car $1) (mplus (cdr $1) $2))]))
+
+(define (bind $ g)
+  (cond
+    [(null? $) mzero]
+    [(procedure? $) (lambda () (bind ($) g))]
+    [(pair? $) (mplus (g (car $)) (bind (cdr $) g))]))
+
+(define-syntax Zzz
+  (syntax-rules ()
+    [(_ g) (lambda (st) (lambda () (g st)))]))
+
+(define-syntax disj+
+  (syntax-rules ()
+    [(_ g) (Zzz g)]
+    [(_ g0 g ...) (disj (Zzz g0) (disj+ g ...))]))
+
+(define-syntax conj+
+  (syntax-rules ()
+    [(_ g) (Zzz g)]
+    [(_ g0 g ...) (conj (Zzz g0) (conj+ g ...))]))
+
+(define-syntax conde
+  (syntax-rules ()
+    [(_ (g0 g ...) ...) (disj+ (conj+ g0 g ...) ...)]))
+
+(define (pull $)
+  (if (procedure? $) (pull ($)) $))
+
+(define (take n $)
+  (if (zero? n) empty
+      (let ([$ (pull $)])
+        (cond
+          [(null? $) $]
+          [else (cons (car $) (take (- n 1) (cdr $)))]))))
+
+(define (take-all $)
+  (let ([$ (pull $)])
+    (if (null? $) $ (cons (car $) (take-all (cdr $))))))
+
+(define (reify-1st st*)
+  (map (reify-var-state (var 0)) st*))
+
+(define ((reify-var-state v) st)
+  (let ([v (walk* v (S-of st))])
+    (walk* v (reify-S v '()))))
+
+
+(define (reify-S v S)
+  (let ([v (walk v S)])
+    (cond
+      [(var? v)
+       (let ([n (reify-name (length S))])
+         (cons '(,v . ,n) S))]
+      [(pair? v) (reify-S (cdr v) (reify-S (car v) S))]
+      [else S]))); number, bool
+
+(define (reify-name n)
+  (string->symbol
+   (string-append "_." (number->string n))))
+
+(define (walk* v S)
+  (let ([v (walk v S)])
+    (cond
+      [(var? v) v]
+      [(pair? v) (cons (walk* (car v) S)
+                       (walk* (cdr v) S))]
+      [else v])))
+
+(define-syntax run*
+  (syntax-rules ()
+    [(_ (x) g0 g ...)
+     (reify-1st (take-all (call/empty-state
+                           (fresh (x) g0 g ...))))]))
+
+(define-syntax run
+  (syntax-rules ()
+    [(_ n (x) g0 g ...)
+     (reify-1st (take n (call/empty-state
+                         (fresh (x) g0 g ...))))]))
+
+(define (call/empty-state g) (g empty-state))
