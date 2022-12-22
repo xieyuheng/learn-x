df2143e868e395199dfd4237c74c6c5e36934236
Xie Yuheng <xyheme@gmail.com>
2022-12-22 15:36:02 +0800

    up

--- learn-datascript/starting-datascript/src/play.clj
+++ learn-datascript/starting-datascript/src/play.clj
@@ -1,17 +1,43 @@
-(ns play
-  (:require [datascript.core :as d]))
+;; (ns play
+;;   (:require [datascript.core :as d]))
 
-(def x "Hello world!")
+(require '[datascript.core :as d])
 
-(println
- (let [schema {:aka {:db/cardinality :db.cardinality/many}}
-       conn   (d/create-conn schema)]
-   (d/transact! conn [ { :db/id -1
-                        :name  "Maksim"
-                        :age   45
-                        :aka   ["Max Otto von Stierlitz", "Jack Ryan"] } ])
-   (d/q '[ :find  ?n ?a
-          :where [?e :aka "Max Otto von Stierlitz"]
-          [?e :name ?n]
-          [?e :age  ?a] ]
-        @conn)))
+(let [schema {:aka {:db/cardinality :db.cardinality/many}}
+      conn (d/create-conn schema)]
+  (d/transact! conn [{:db/id -1
+                      :name "Maksim"
+                      :age 45
+                      :aka ["Max Otto von Stierlitz", "Jack Ryan"]}])
+  (d/q '[:find ?n ?a
+         :where
+         [?e :aka "Max Otto von Stierlitz"]
+         [?e :name ?n]
+         [?e :age ?a]]
+       @conn))
+
+(d/q '[:find ?k ?x
+       :in [[?k [?min ?max]] ...] ?range
+       :where
+       [(?range ?min ?max) [?x ...]]
+       [(even? ?x)]]
+     {:a [1 7], :b [2 4]}
+     range)
+
+(d/q '[:find ?u1 ?u2
+       :in $ %
+       :where (follows ?u1 ?u2)]
+     [[1 :follows 2]
+      [2 :follows 3]
+      [3 :follows 4]]
+     '[[(follows ?e1 ?e2)
+        [?e1 :follows ?e2]]
+       [(follows ?e1 ?e2)
+        [?e1 :follows ?t]
+        (follows ?t ?e2)]])
+
+(d/q '[:find ?color (max ?amount ?x) (min ?amount ?x)
+       :in [[?color ?x]] ?amount]
+     [[:red 10] [:red 20] [:red 30] [:red 40] [:red 50]
+      [:blue 7] [:blue 8]]
+     3)
