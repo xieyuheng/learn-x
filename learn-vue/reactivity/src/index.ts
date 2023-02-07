62fa77f3b1746d222cafa754a93d8af435f2b2b1
Xie Yuheng <xyheme@gmail.com>
2023-02-08 00:54:56 +0800

    up

--- learn-vue/reactivity/src/index.ts
+++ learn-vue/reactivity/src/index.ts
@@ -1,7 +1,9 @@
-import { reactive } from "@vue/reactivity"
+import { effect, reactive } from "@vue/reactivity"
 
 export function reactiveState() {
   const state = reactive({ count: 0 })
 
-  return state
+  effect
+
+  return { state }
 }
