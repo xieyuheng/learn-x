8a1f900838ea75ba7f860d67b96aedbd34d9f97e
Xie Yuheng <xyheme@gmail.com>
2023-02-22 03:47:59 +0800

    up

--- learn-nodejs/net/log.mjs
+++ learn-nodejs/net/log.mjs
@@ -2,9 +2,16 @@ import net from "node:net"
 
 const server = net.createServer((socket) => {
   socket.write("Hi, I will log your messages to the server.")
+
   socket.on("data", (data) => {
     process.stdout.write(data.toString())
   })
 })
 
-server.listen(8080)
+server.on("connection", (socket) => {
+  console.log(socket.address())
+})
+
+server.listen({ port: 8080 }, () => {
+  console.log(server.address())
+})
