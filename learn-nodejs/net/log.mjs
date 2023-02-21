5c001160796bcc927d1990fc142d00c80a3cf5b9
Xie Yuheng <xyheme@gmail.com>
2023-02-22 04:03:54 +0800

    up

--- learn-nodejs/net/log.mjs
+++ learn-nodejs/net/log.mjs
@@ -1,6 +1,15 @@
 import net from "node:net"
 
 const server = net.createServer((socket) => {
+  console.log({
+    localAddress: socket.localAddress,
+    localPort: socket.localPort,
+    localFamily: socket.localFamily,
+    remoteAddress: socket.remoteAddress,
+    remotePort: socket.remotePort,
+    remoteFamily: socket.remoteFamily,
+  })
+
   socket.write("Hi, I will log your messages to the server.")
 
   socket.on("data", (data) => {
@@ -8,10 +17,8 @@ const server = net.createServer((socket) => {
   })
 })
 
-server.on("connection", (socket) => {
-  console.log(socket.address())
-})
-
 server.listen({ port: 8080 }, () => {
-  console.log(server.address())
+  console.log({
+    "server.address": server.address(),
+  })
 })
