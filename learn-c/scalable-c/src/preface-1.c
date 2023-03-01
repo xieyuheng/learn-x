ca2fdc6242b682fd54e1bbeec1a1ee741bdc8278
Xie Yuheng <xyheme@gmail.com>
2023-03-01 09:00:14 +0800

    up

--- learn-c/scalable-c/src/preface-1.c
+++ learn-c/scalable-c/src/preface-1.c
@@ -1,19 +1,21 @@
 #include <czmq.h>
 
-int main (void) {
-  zlist_t *list = zlist_new ();
-  zlist_append (list, "apple");
-  zlist_append (list, "orange");
-  zlist_push (list, "grape");
-  zlist_push (list, "tomato");
-  printf ("%zd: ", zlist_size (list));
+int main(void) {
+  zlist_t *list = zlist_new();
+  zlist_append(list, "apple");
+  zlist_append(list, "orange");
+  zlist_push(list, "grape");
+  zlist_push(list, "tomato");
+  printf("%zd: ", zlist_size(list));
 
-  char *fruit = (char *) zlist_first (list);
+  char *fruit = (char *) zlist_first(list);
+
   while (fruit) {
-    printf ("%s ", fruit);
-    fruit = (char *) zlist_next (list);
+    printf("%s ", fruit);
+    fruit = (char *) zlist_next(list);
   }
-  puts ("");
-  zlist_destroy (&list);
+
+  puts("");
+  zlist_destroy(&list);
   return 0;
 }
--- learn-zmq/zguide-c/TODO.md
+++
@@ -1,1 +0,0 @@
-copy code from zguide repo
