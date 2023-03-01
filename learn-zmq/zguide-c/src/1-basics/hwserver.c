d6ddeb8f0a252a00202e9dea6ddad8d6a31b4152
Xie Yuheng <xyheme@gmail.com>
2023-03-01 09:26:01 +0800

    up

---
+++ learn-zmq/zguide-c/src/1-basics/hwserver.c
@@ -0,0 +1,26 @@
+//  Hello World server
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <zmq.h>
+
+int main(void) {
+    //  Socket to talk to clients
+    void *context = zmq_ctx_new();
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind(responder, "tcp://*:5555");
+    assert(rc == 0);
+
+    while (1) {
+        char buffer[10];
+        zmq_recv(responder, buffer, 10, 0);
+        printf("Received Hello\n");
+        //  Do some 'work'
+        sleep(1);
+        zmq_send(responder, "World", 5, 0);
+    }
+
+    return 0;
+}
