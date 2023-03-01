dd196390b1c87303189d9116e4678682a88f1816
Xie Yuheng <xyheme@gmail.com>
2023-03-01 08:29:26 +0800

    up

---
+++ learn-zmq/zguide-c/play/p1.c
@@ -0,0 +1,19 @@
+#include <czmq.h>
+
+int main (void) {
+  zlist_t *list = zlist_new ();
+  zlist_append (list, "apple");
+  zlist_append (list, "orange");
+  zlist_push (list, "grape");
+  zlist_push (list, "tomato");
+  printf ("%zd: ", zlist_size (list));
+
+  char *fruit = (char *) zlist_first (list);
+  while (fruit) {
+    printf ("%s ", fruit);
+    fruit = (char *) zlist_next (list);
+  }
+  puts ("");
+  zlist_destroy (&list);
+  return 0;
+}
