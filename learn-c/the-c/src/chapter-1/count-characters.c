d344b29428b1526934784f55d4150a1ab42e82e3
Xie Yuheng <xyheme@gmail.com>
2023-07-25 15:26:14 +0800

    up

---
+++ learn-c/the-c/src/chapter-1/count-characters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* count characters in input; 2nd version */
+int main()
+{
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+}
