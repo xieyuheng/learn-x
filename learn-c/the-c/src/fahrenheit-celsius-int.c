1e68aeffb75ddc3a5226e921258dce1b0786b915
Xie Yuheng <xyheme@gmail.com>
2023-07-25 09:15:14 +0800

    up

--- learn-c/the-c/src/fahrenheit-celsius-int.c
+++ learn-c/the-c/src/fahrenheit-celsius-int.c
@@ -14,7 +14,8 @@ int main() {
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        // printf("%d\t%d\n", fahr, celsius);
+        printf("%3d %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
