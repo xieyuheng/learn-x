8a3b1bf26dc02ec43487cccb17f6fcd7e4e120a9
Xie Yuheng <xyheme@gmail.com>
2023-07-25 15:51:15 +0800

    up

---
+++ learn-c/the-c/src/chapter-1/count-line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* count lines in input */
+int main()
+{
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+}
