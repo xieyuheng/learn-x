c12f9454f885fa45ba321e215df6a2bb800e1e9c
Xie Yuheng <xyheme@gmail.com>
2023-08-17 16:50:17 +0800

    up

---
+++ learn-zig/ziglearn/1.zig
@@ -0,0 +1,12 @@
+const expect = @import("std").testing.expect;
+
+fn failingFunction() !void {
+    return error.Oops;
+}
+
+test "returning an error" {
+    failingFunction() catch |err| {
+        try expect(err == error.Oops);
+        return;
+    };
+}
