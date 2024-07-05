48c9bfcbfa50c67deb3f8e63b5448115ae7efb90
Xie Yuheng <xyheme@gmail.com>
2024-07-05 18:02:22 +0800

    画线

--- learn-threejs/starting-threejs/src/main.ts
+++ learn-threejs/starting-threejs/src/main.ts
@@ -3,29 +3,34 @@ import { assertWebGL } from "./assertWebGL.js"
 
 assertWebGL()
 
-const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
+  1,
+  500,
 )
+camera.position.set(0, 0, 100)
+camera.lookAt(0, 0, 0)
+// camera.position.z = 5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setAnimationLoop(animate)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00eeaa })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-camera.position.z = 5
+const scene = new THREE.Scene()
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const line = new THREE.Line( geometry, material );
+scene.add(line)
 
 function animate() {
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+line.rotation.x += 0.01
+line.rotation.y += 0.01
 
   renderer.render(scene, camera)
 }
