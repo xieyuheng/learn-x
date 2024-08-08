3f5ca163872572f053b8fdc8d4b5942c59b4b31c
Xie Yuheng <xyheme@gmail.com>
2024-08-08 17:26:14 +0800

    up

--- learn-prolog/starting-prolog/src/list_length.pl
+++ learn-prolog/starting-prolog/src/list_length.pl
@@ -2,9 +2,9 @@
 
 list_length([], 0).
 list_length([_|Ls], N) :-
-        N #> 0,
-        N #= N0 + 1,
-        list_length(Ls, N0).
+  N #> 0,
+  N #= N0 + 1,
+  list_length(Ls, N0).
 
 :- initialization(main).
 
