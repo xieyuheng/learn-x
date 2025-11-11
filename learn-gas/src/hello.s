6bf42b5b64da417aa0eb4caa75863b2e986cc388
Xie Yuheng <xyheme@gmail.com>
2025-11-12 03:49:23 +0800

    up

--- learn-gas/src/hello.s
+++ learn-gas/src/hello.s
@@ -4,7 +4,7 @@
 
 message:
         .string "Hello, World!\n"
-        length = . - message
+        message.length = . - message
 
         .text
 
@@ -12,7 +12,7 @@ _start:
         movq $1, %rax
         movq $1, %rdi
         movq $message, %rsi
-        movq $length, %rdx
+        movq $message.length, %rdx
         syscall
 
         movq $60, %rax
